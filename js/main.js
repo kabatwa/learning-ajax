7c903990cd959ee33f3e690e549764d13a63db7f
Timothy Bryan <tim@orangesauce.org>
2014-02-18 18:55:51 +0000

    Finished Handling XML Responses

--- js/main.js
+++ js/main.js
@@ -6,16 +6,31 @@
     xhr.onreadystatechange=function(){
         if((xhr.readyState==4)&&(xhr.status==200 || xhr.status==304)){
           var body=document.getElementsByTagName("body")[0];
-          var div=document.createElement("div");
-          body.appendChild(div);
-          var divv=document.getElementsByTagName("div")[0];
-          divv.innerHTML=xhr.responseText;
+          var heading=xhr.responseXML.getElementsByTagName("heading")[0].firstChild.nodeValue;
+          var h2=document.createElement("h2");
+          var h2Text=document.createTextNode(heading);
+          h2.appendChild(h2Text);
+
+          var list=document.createElement("ul");
+          var items=xhr.responseXML.getElementsByTagName("items")[0];
+
+          items=items.getElementsByTagName("item");
+          for(var i=0;i<items.length;i++){
+            var item=items[i].firstChild.nodeValue;
+            var li=document.createElement("li");
+            var liText=document.createTextNode(item);
+            li.appendChild(liText);
+            list.appendChild(li);
+          }
+
+          body.appendChild(h2);
+          body.appendChild(list);
 
           body.removeChild(link);
         }
     };
 
-    xhr.open("GET","files/ajax.html",true);
+    xhr.open("GET","files/ajax.xml",true);
     xhr.send(null);
 
     return false;
